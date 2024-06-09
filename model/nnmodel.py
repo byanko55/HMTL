8e904f2be8a64e98a04d4af2df89bd852410af13
byanko55 <byanko55@gmail.com>
2024-06-10 00:00:37 +0900

    convolutional neural networks for MNIST/SVHN/MNIST-M

--- model/nnmodel.py
+++ model/nnmodel.py
@@ -2,6 +2,7 @@
 Pytorch Neural Net Classes
 """
 from model.nnloader import *
+import torch.nn as nn
 
 import os
 
@@ -23,5 +24,84 @@ class DigitNN(torch.nn.Module):
     def forward(self, input_data:torch.tensor) -> torch.tensor:
         """ Inference """
         y = self.nn(input_data)
+
+        return y
+    
+
+def build_fc_classifier(dim_in, num_classes=10):
+    """ 3 Fully Connected Neural Networks """
+    fc = nn.Sequential()
+    fc.add_module('fc1', nn.Linear(dim_in, 128))
+    fc.add_module('fc2', nn.Linear(128, 64))
+    fc.add_module('fc3', nn.Linear(64, num_classes))
+    fc.add_module('softmax', nn.LogSoftmax(dim = 1))
+        
+    return fc
+
+
+def build_conv_net(in_channel, init_padding="same"):
+    """ 3 Convolutional Neural Networks """
+    conv = nn.Sequential()
+    conv.add_module('conv1', nn.Conv2d(in_channel, 32, kernel_size=5, padding=init_padding))
+    conv.add_module('pool1', nn.MaxPool2d(2))
+    conv.add_module('relu1', nn.ReLU(True))
+    conv.add_module('conv2', nn.Conv2d(32, 32, kernel_size=3, padding="same"))
+    conv.add_module('drop1', nn.Dropout2d(p=0.1))
+    conv.add_module('pool2', nn.MaxPool2d(2))
+    conv.add_module('relu2', nn.ReLU(True))
+    conv.add_module('conv3', nn.Conv2d(32, 32, kernel_size=3, padding="same"))
+    conv.add_module('pool3', nn.MaxPool2d(2))
+    conv.add_module('relu3', nn.ReLU(True))
+    
+    return conv
+
+
+class CNN_MNIST(nn.Module):
+    """ CNN model for MNIST dataset """
+    def __init__(self):
+        super(CNN_MNIST, self).__init__()
+
+        self.convL = build_conv_net(in_channel=1, init_padding="same")
+        self.fcL = build_fc_classifier(dim_in=32 * 3 * 3, num_classes=10)
+
+    def forward(self, input_data):
+        """ Inference """
+        x = self.convL(input_data)
+        x = x.view(-1, 3 * 3 * 32)
+        y = self.fcL(x)
+
+        return y
+    
+
+class CNN_SVHN(nn.Module):
+    """ CNN model for SVHN dataset """
+    def __init__(self):
+        super(CNN_SVHN, self).__init__()
+
+        self.convL = build_conv_net(in_channel=3, init_padding="valid")
+        self.fcL = build_fc_classifier(dim_in=32 * 3 * 3, num_classes=10)
+
+    def forward(self, input_data):
+        """ Inference """
+        x = self.convL(input_data)
+        x = x.view(-1, 3 * 3 * 32)
+        y = self.fcL(x)
+
+        return y
+    
+
+class CNN_MNISTM(nn.Module):
+    """ CNN model for MNIST-M dataset """
+    def __init__(self):
+        super(CNN_MNISTM, self).__init__()
+        
+        self.convL = build_conv_net(in_channel=3, init_padding="same")
+        self.fcL = build_fc_classifier(dim_in=32 * 3 * 3, num_classes=10)
+
+    def forward(self, input_data):
+        """ Inference """
+        x = self.convL(input_data)
+        x = x.view(-1, 3 * 3 * 32)
+        y = self.fcL(x)
 
         return y
